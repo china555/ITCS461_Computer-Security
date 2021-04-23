c15c8bcb84eee8649869d62ec1613516db3cd7eb
Chinapanda <kittikornchina2014@gmail.com>
2021-04-24 02:03:40 +0700

    adding length input

--- project/src/components/Converter.jsx
+++ project/src/components/Converter.jsx
@@ -4,14 +4,20 @@ import "../App.css";
 export const Converter = () => {
   const [encryptionValue, setEncryptionValue] = useState("");
   const [decryptionValue, setDecryptionValue] = useState("");
-
+  const [selectLength, setSelectLength] = useState(1);
+  const maxlength = 122;
+  const minlength = 97;
   const Decryption = (data) => {
     let result = "";
+
     for (let i = 0; i < data.length; i++) {
-      if (data[i].charCodeAt() + 5 > 122) {
-        result += data[i].charCodeAt() - 122 + 5;
+      if (data[i].charCodeAt() + 3 > maxlength) {
+        result += String.fromCharCode(
+          96 + data[i].charCodeAt() - maxlength + 3
+        );
+      } else {
+        result += String.fromCharCode(data[i].charCodeAt() + 3);
       }
-      result += String.fromCharCode(data[i].charCodeAt() + 5);
     }
     setDecryptionValue(result);
   };
@@ -19,7 +25,13 @@ export const Converter = () => {
   const Encryption = (data) => {
     let result = "";
     for (let i = 0; i < data.length; i++) {
-      result += String.fromCharCode(data[i].charCodeAt() - 5);
+      if (data[i].charCodeAt() - 3 < minlength) {
+        result += String.fromCharCode(
+          123 + data[i].charCodeAt() - minlength - 3
+        );
+      } else {
+        result += String.fromCharCode(data[i].charCodeAt() - 3);
+      }
     }
     setEncryptionValue(result);
   };
